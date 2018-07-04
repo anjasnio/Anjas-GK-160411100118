27af73cf6e4a58fc1973a6836fd72ae6c45da4e5
anjasnio <37510359+anjasnio@users.noreply.github.com>
2018-07-04 20:43:55 +0700

    fourth commit

--- Modul4/Nomer5/main.cpp
+++ Modul4/Nomer5/main.cpp
@@ -1,1 +1,98 @@
 
+/*
+ * GLUT Shapes Demo
+ *
+ * Written by Nigel Stewart November 2003
+ *
+ * This program is test harness for the sphere, cone
+ * and torus shapes in GLUT.
+ *
+ * Spinning wireframe and smooth shaded shapes are
+ * displayed until the ESC or q key is pressed.  The
+ * number of geometry stacks and slices can be adjusted
+ * using the + and - keys.
+ */
+
+#include <windows.h>
+#ifdef __APPLE__
+#include <GLUT/glut.h>
+#else
+#include <GL/glut.h>
+#endif
+
+#include <stdlib.h>
+#include <Math.h>
+
+void display()
+{
+    glClear(GL_COLOR_BUFFER_BIT);
+    glColor3f(0.0,0.0,0.8);
+    glRecti(-2, 12, 2, 8);
+
+    glTranslated(5, -1, 0);
+    glRecti(-1, 11, 1, 9);
+
+    glTranslated(3, -4, 0);
+    glRecti(-2, 12, 2, 8);
+
+    glTranslated(2, -5, 0);
+    glRecti(-1, 11, 1, 9);
+
+    glTranslated(-2, -5, 0);
+    glRecti(-2, 12, 2, 8);
+
+    glTranslated(-3, -4, 0);
+    glRecti(-1, 11, 1, 9);
+
+    glTranslated(-5, -1, 0);
+    glRecti(-2, 12, 2, 8);
+
+    glTranslated(-5, 1, 0);
+    glRecti(-1, 11, 1, 9);
+
+    glTranslated(-3, 4, 0);
+    glRecti(-2, 12, 2, 8);
+
+    glTranslated(-2, 5, 0);
+    glRecti(-1, 11, 1, 9);
+
+    glTranslated(2, 5, 0);
+    glRecti(-2, 12, 2, 8);
+
+    glTranslated(3, 4, 0);
+    glRecti(-1, 11, 1, 9);
+
+    glLoadIdentity();
+
+
+    glBegin(GL_LINE_LOOP);
+    glColor3f(1.0, 1.0, 0.0);
+    for(float t = 0.0; t<=6.28; t+=0.01)
+    {
+        glVertex2f(10*cos(t), 10*sin(t));
+
+    }
+    glEnd();
+    glFlush();
+}
+void myinit()
+{
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluOrtho2D(-13.0, 13.0, -13.0, 13.0);
+    glMatrixMode(GL_MODELVIEW);
+    glClearColor(0.0,0.0,0.0,0.0);
+    glColor3f(0.0,0.0,0.0);
+}
+int main(int argc, char* argv[])
+{
+    glutInit(&argc,argv);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+    glutInitWindowSize(400,400);
+    glutInitWindowPosition(100,100);
+    glutCreateWindow("Transform");
+    glutDisplayFunc(display);
+    myinit();
+    glutMainLoop();
+    return 0;
+}
